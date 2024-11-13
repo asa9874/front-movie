a6676bc36ce6c725ef74a70313db60a6df600a06
asa9874 <pjkpjk1129@gmail.com>
2024-11-13 22:05:33 +0900

    Feat:Header

---
+++ src/header.js
@@ -0,0 +1,4 @@
+import logo from './assets/logo.png';
+
+const logoImg = document.querySelector('.logo');
+logoImg.src = logo;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import "./styles/global.css";
+import "./styles/header.css";
+import "./styles/main.css";
+import "./styles/reset.css";
+
+import "./header.js";
