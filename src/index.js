acdee40b974f51bd8e4c132d319bc6ea9286e8bf
asa9874 <pjkpjk1129@gmail.com>
2024-11-17 18:48:48 +0900

    Feat: 축소 버튼

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import "./styles/global.css";
 import "./styles/header.css";
-import "./styles/main.css";
+import "./styles/movie.css";
 import "./styles/reset.css";
 
 import "./events/ButtonEvent.ts";
